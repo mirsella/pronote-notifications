c54e2b4cc98dfc5450e5f330050da505bc98d166
mirsella <mirsella@protonmail.com>
2021-01-05 11:07:32 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+it may not work for every instance of pronote since some of them probably differ from mine
---
+++ index.js
@@ -0,0 +1,42 @@
+const puppeteer = require('puppeteer');
+const chalk = require('chalk')
+require('dotenv').config()
+
+const error = (text) => {
+  console.log(chalk.redBright.bold(text))
+  process.exit(1)
+} 
+
+// checking environment variables
+process.env.url || error("didn't provided a pronote url in .env !");
+process.env.username || error("didn't provided a pronote username in .env !");
+process.env.password || error("didn't provided a pronote password in .env !");
+
+(async () => {
+  const browser = await puppeteer.launch({headless: true})
+  const page = await browser.newPage()
+  await page.goto(process.env.url, {waitUntil: 'networkidle0', timeout: 60000})
+  await page.type('#id_50', process.env.username)
+  await page.type('#id_51', process.env.password)
+  await page.click('#id_39')
+  await new Promise(resolve => {
+    page.on('response', res => {
+      res.text().then(text => {
+        if (text.includes("notificationsCommunication")) {
+          res.json().then(json => {
+            let notificationsCommunication = json.donneesSec._Signature_.notificationsCommunication
+            let onglet = { 131: "discussions", 104: "i don't know", 8: "informations" }
+            response = {}
+            notificationsCommunication.forEach(item => {
+              response[onglet[item.onglet]] = item.nb
+            })
+            console.log(response)
+          })
+          resolve()
+        }
+      })
+    }) 
+  })
+  await browser.close()
+})();
+
