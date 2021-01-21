1aa8a5d101d8fe074d39a93d81aefdcc5cfff655
mirsella <mirsella@protonmail.com>
2021-01-21 20:16:25 +0100

    gacp Thu Jan 21 08:16:25 PM CET 2021

--- wrapper.bash
+++ wrapper.bash
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 cd /home/mirsella/pronote-notifications
 messages=$(node index.js)
 [ ! -f last.json ] && echo '{ "discussions": 0, "others": 0, "informations": 0, "last": 0 }' > last.json
@@ -10,7 +10,7 @@ nMessages=$(jq -j '.informations, .discussions, .others' <<< $messages)
 nLast=$(jq -j '.informations, .discussions, .others' <<< $last)
 
 if ! grep -q '000' <<< $nMessages; then 
-  if [ $lasttime -gt 36 ]; then
+  if grep -a '^[0-9]*$' <<< $((lasttime/36)); then
     notif "Still Got Notifications ! $nMessages"
   elif grep -q '000' <<< $nLast; then 
     notif "New Notifications ! $nMessages"
