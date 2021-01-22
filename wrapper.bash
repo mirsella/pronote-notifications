e738b4b1bf52b45f3c70741c0f795fbd3789b824
mirsella <mirsella@protonmail.com>
2021-01-22 15:33:13 +0100

    Update wrapper.bash

--- wrapper.bash
+++ wrapper.bash
@@ -3,14 +3,14 @@ cd /home/mirsella/pronote-notifications
 messages=$(node index.js || notif 'error pronote-notifications')
 [ ! -f last.json ] && echo '{ "discussions": 0, "others": 0, "informations": 0, "last": 0 }' > last.json
 last=$(cat last.json)
-lasttime=$(jq '.last' <<< $last)
-((lasttime++))
+count=$(jq '.last' <<< $last)
+((count++))
 
 nMessages=$(jq -j '.informations, .discussions, .others' <<< $messages)
 nLast=$(jq -j '.informations, .discussions, .others' <<< $last)
 
 if ! grep -q '000' <<< $nMessages; then 
-  if bc -l <<< "$lasttime/36" | grep -q '^[0-9]*\.*00000000000000000000$'; then
+  if bc -l <<< "$count/16" | grep -q '^[0-9]*\.*00000000000000000000$'; then
     notif "Still Got Notifications ! $nMessages"
   elif grep -q '000' <<< $nLast; then 
     notif "New Notifications ! $nMessages"
